b37baca42e2807585e3e1b7d56b98906d5f77660
MatheusRafaelDaCostaVieira <matheusrafael.pa@gmail.com>
2023-09-19 20:28:54 -0300

    Menu javascript functions almost.

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    var menu = document.getElementById('menu')
+    menu.style.display= "block"
+}
+function closeMenu() {
+    var menu = document.getElementById('menu')
+    menu.style.display= "none"
+}
