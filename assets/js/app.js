fdd1154ec52f9ecf1d733d2a066df6b9a9949815
MatheusRafaelDaCostaVieira <matheusrafael.pa@gmail.com>
2023-09-23 13:26:12 -0300

    Navbar animation js code done.

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,17 @@ function closeMenu() {
     var menu = document.getElementById('menu')
     menu.classList.remove("open")
 }
+
+window.addEventListener("scroll", navbarAnimation)
+
+function navbarAnimation() {
+    var navbar = document.getElementById('nav')
+    const scroll = window.scrollY
+
+    if(scroll > 0) {
+        navbar.classList.remove("scroll")
+    }
+    else {
+        navbar.classList.add("scroll")
+    }
+}
